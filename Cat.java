5718350c87af4bbc71f5fb685049c7c2a1e4f0a7
Hiba ERRAOUI <125480922+er-hiba@users.noreply.github.com>
2023-12-17 20:00:58 +0100

    :)

---
+++ Cat.java
@@ -0,0 +1,12 @@
+// Define a Cat class extending the Animal class
+class Cat extends Animal {
+    // Constructor
+    public Cat(String name, int age) {
+        super(name, age);
+    }
+
+    // Implementing the sound method for a Cat
+    public String sound() {
+        return "Meowing";
+    }
+}
